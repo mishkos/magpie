3d68a11b24d5c9dbffa487dcb0ae6e6457d16a89
Miodrag Stevanovic <stevanovic@pik-potsdam.de>
2018-12-04 12:22:58 +0100

    new starting scripts for the runs at the iamc conference

---
+++ scripts/start/iamc_ssp_runs_full_ndc.R
@@ -0,0 +1,150 @@
+# (C) 2008-2017 Potsdam Institute for Climate Impact Research (PIK),
+# authors, and contributors see AUTHORS file
+# This file is part of MAgPIE and licensed under GNU AGPL Version 3
+# or later. See LICENSE file or go to http://www.gnu.org/licenses/
+# Contact: magpie@pik-potsdam.de
+
+
+######################################
+#### Script to start a MAgPIE run ####
+######################################
+
+library(lucode)
+library(magclass)
+library(magpie4)
+
+source("config/default.cfg")
+source("scripts/start_functions.R")
+
+# H12
+regdef <- "_8a828c6ed5004e77d1ba2025e8ea2261"
+# H11
+# regdef <- "_690d3718e151be1b450b394c1064b1c5"
+
+cfg$input[1] <- paste0("isimip_rcp-IPSL_CM5A_LR-rcp2p6-co2_rev34_c200",regdef,".tgz")
+cfg$input[2] <- paste0("rev4.14",regdef,"_magpie.tgz")
+cfg$input[3] <- paste0("rev4.14",regdef,"_validation.tgz")
+cfg$input[4] <- "additional_data_rev3.59.tgz"
+# cfg$input[5] <- "calibration_H11_23Oct18.tgz"
+cfg$input[5] <- "calibration_H12_NPI_06Nov18.tgz"
+
+ssps <- c("SSP1","SSP2","SSP3","SSP4","SSP5")
+
+cfg$force_download <- FALSE
+cfg$recalc_npi_ndc <- TRUE
+
+cfg$gms$s80_maxiter <- 30
+cfg$output <- c("report","validation")
+
+# cfg$recalibrate <- FALSE
+
+for(ssp in ssps){
+  cfg$results_folder <- paste0("output/",ssp,"/:title::date:")
+
+  # cfg$title <- paste0(ssp,"_BASE_npi")
+  # cfg <- setScenario(cfg,c(ssp,"BASE"))
+  # start_run(cfg,codeCheck=FALSE)
+
+  # cfg$title <- paste0(ssp,"_NPI")
+  # cfg <- setScenario(cfg,c(ssp,"NPI"))
+  # start_run(cfg,codeCheck=FALSE)
+
+  cfg$title <- paste0(ssp,"_NDC")
+  cfg <- setScenario(cfg,c(ssp,"NDC"))
+  start_run(cfg,codeCheck=FALSE)
+
+  # cfg$recalc_npi_ndc <- FALSE
+
+  # ### Runs with CC
+  #
+  # #CC
+  # cfg$gms$c14_yields_scenario  <- "cc"
+  # cfg$gms$c42_watdem_scenario  <- "cc"
+  # cfg$gms$c43_watavail_scenario  <- "cc"
+  # cfg$gms$c52_carbon_scenario  <- "cc"
+  #
+  # res <- "c200"
+  #
+  # cfg$title <- paste(ssp,"Ref_RCP60_co2_BASE",res,sep="_")
+  # cfg <- setScenario(cfg,c(ssp,"BASE"))
+  # cfg$input[1] <- paste0("isimip_rcp-IPSL_CM5A_LR-rcp6p0-co2_rev34_",res,regdef,".tgz")
+  # start_run(cfg,codeCheck=FALSE)
+  #
+  # cfg$title <- paste(ssp,"Ref_RCP60_co2_NDC",res,sep="_")
+  # cfg <- setScenario(cfg,c(ssp,"NDC"))
+  # cfg$input[1] <- paste0("isimip_rcp-IPSL_CM5A_LR-rcp6p0-co2_rev34_",res,regdef,".tgz")
+  # start_run(cfg,codeCheck=FALSE)
+}
+
+
+#-------------------------------------------------------------------------------
+
+# full ad ndc policy -----------------------------------------------------------
+
+# read from policy_definitions_all_ad.csv file
+# MANUAL SETUP
+
+# cfg$title <- "SSP2_NDC_fullAD"
+# cfg <- setScenario(cfg,c("SSP2","NDC"))
+# cfg$recalc_npi_ndc <- TRUE
+# start_run(cfg,codeCheck=FALSE)
+
+# cfg$title <- "SSP2_NDC_fullAD_fullAOLC"
+# cfg <- setScenario(cfg,c("SSP2","NDC"))
+# cfg$recalc_npi_ndc <- TRUE
+# start_run(cfg,codeCheck=FALSE)
+
+
+#
+#
+# ## rcp26
+#
+# cfg$title <- paste("SSP2_Ref_RCP26_co2_BASE",res,sep="_")
+# cfg <- setScenario(cfg,c("SSP2","BASE"))
+# cfg$gms$c56_pollutant_prices <- "SSP2-Ref-SPA0-V15-REMIND-MAGPIE"
+# cfg$gms$c60_2ndgen_biodem <- "SSP2-Ref-SPA0"
+# cfg$input[1] <- paste0("isimip_rcp-IPSL_CM5A_LR-rcp2p6-co2_rev34_",res,regdef,".tgz")
+# start_run(cfg,codeCheck=FALSE)
+#
+# cfg$title <- paste("SSP2_Ref_RCP26_co2_NPI",res,sep="_")
+# cfg <- setScenario(cfg,c("SSP2","NPI"))
+# cfg$gms$c56_pollutant_prices <- "SSP2-Ref-SPA0-V15-REMIND-MAGPIE"
+# cfg$gms$c60_2ndgen_biodem <- "SSP2-Ref-SPA0"
+# cfg$input[1] <- paste0("isimip_rcp-IPSL_CM5A_LR-rcp2p6-co2_rev34_",res,regdef,".tgz")
+# start_run(cfg,codeCheck=FALSE)
+#
+# cfg$title <- paste("SSP2_Ref_RCP26_co2_NDC",res,sep="_")
+# cfg <- setScenario(cfg,c("SSP2","NDC"))
+# cfg$gms$c56_pollutant_prices <- "SSP2-Ref-SPA0-V15-REMIND-MAGPIE"
+# cfg$gms$c60_2ndgen_biodem <- "SSP2-Ref-SPA0"
+# cfg$input[1] <- paste0("isimip_rcp-IPSL_CM5A_LR-rcp2p6-co2_rev34_",res,regdef,".tgz")
+# start_run(cfg,codeCheck=FALSE)
+#
+# cfg$title <- paste("SSP2_Ref_RCP26_co2_NPI_exotc_base",res,sep="_")
+# cfg <- setScenario(cfg,c("SSP2","NPI"))
+# cfg$gms$c56_pollutant_prices <- "SSP2-Ref-SPA0-V15-REMIND-MAGPIE"
+# cfg$gms$c60_2ndgen_biodem <- "SSP2-Ref-SPA0"
+# cfg$input[1] <- paste0("isimip_rcp-IPSL_CM5A_LR-rcp2p6-co2_rev34_",res,regdef,".tgz")
+# cfg$gms$tc <- "exo_oct18"
+# cfg$gms$c13_tau_scen <- "base"
+# start_run(cfg,codeCheck=FALSE)
+#
+# cfg$title <- paste("SSP2_Ref_RCP26_co2_NDC_exotc_base",res,sep="_")
+# cfg <- setScenario(cfg,c("SSP2","NDC"))
+# cfg$gms$c56_pollutant_prices <- "SSP2-Ref-SPA0-V15-REMIND-MAGPIE"
+# cfg$gms$c60_2ndgen_biodem <- "SSP2-Ref-SPA0"
+# cfg$input[1] <- paste0("isimip_rcp-IPSL_CM5A_LR-rcp2p6-co2_rev34_",res,regdef,".tgz")
+# cfg$gms$tc <- "exo_oct18"
+# cfg$gms$c13_tau_scen <- "base"
+# start_run(cfg,codeCheck=FALSE)
+#
+# cfg$title <- paste("SSP2_Ref_RCP26_co2_NDC_exotc_npi",res,sep="_")
+# cfg <- setScenario(cfg,c("SSP2","NDC"))
+# cfg$gms$c56_pollutant_prices <- "SSP2-Ref-SPA0-V15-REMIND-MAGPIE"
+# cfg$gms$c60_2ndgen_biodem <- "SSP2-Ref-SPA0"
+# cfg$input[1] <- paste0("isimip_rcp-IPSL_CM5A_LR-rcp2p6-co2_rev34_",res,regdef,".tgz")
+# cfg$gms$tc <- "exo_oct18"
+# cfg$gms$c13_tau_scen <- "npi"
+# start_run(cfg,codeCheck=FALSE)
+#
+# cfg$gms$tc <- "endo_jun18"
